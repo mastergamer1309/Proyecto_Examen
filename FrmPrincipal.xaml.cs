88d85b28941f4e672c9de7959fde0b38c9a859ab
mastergamer1309 <josiaszeledon20@gmail.com>
2024-06-29 12:32:02 -0600

    creacion de pagina principoal

---
+++ FrmPrincipal.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_Examen
+{
+    /// <summary>
+    /// Interaction logic for FrmPrincipal.xaml
+    /// </summary>
+    public partial class FrmPrincipal : Page
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNuevoAnalisis_Click(object sender, RoutedEventArgs e)
+        {
+            FrmCalculos frmcalculos = new FrmCalculos();
+            frmcalculos.Show();
+        }
+    }
+}
